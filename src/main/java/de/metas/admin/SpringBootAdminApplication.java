2bbb218e8adb27b7ce31f9057b9eed410b61b647
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-09-05 08:50:41 +0200

    #3787-app Upgrade to spring-boot 2.0.4
    Upgrade to spring 5.0.4.RELEASE https://github.com/metasfresh/metasfresh/issues/3787

--- src/main/java/de/metas/admin/SpringBootAdminApplication.java
+++ src/main/java/de/metas/admin/SpringBootAdminApplication.java
@@ -3,7 +3,7 @@ package de.metas.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 
 
